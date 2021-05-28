185c85457af5bc55a33b65d87712747dc1c6b3e4
Techika <adibukovi@gmail.com>
2021-05-28 11:40:56 +0200

    setState based hiding alternative

--- brepi-buk/src/tiles/TileFrame.tsx
+++ brepi-buk/src/tiles/TileFrame.tsx
@@ -1,6 +1,6 @@
 import BeerTile from "./BeerTile";
 import TileFace from "./TileInterface";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import logo from "../logo.svg";
 
 export interface TileFrameProps {
@@ -12,11 +12,23 @@ const TileFrame: React.FC<TileFrameProps> = (props) => {
   //   for (let a = 6 - props.tilesToShow.length ;a ===0 ; a--){
 
   //   }
+  // const hiddenTile = useRef(null);
+  function beerClicked(e: any, id: string): void {
+    setHiddenTile(id);
+  }
+  const [hiddenTile, setHiddenTile] = useState("");
   return (
     <div id="tile-frame" key="beer-matrix">
       {props.tilesToShow.map((tile: TileFace) => {
         // console.log("Tileinfo:", tile);
-        return <BeerTile tileInfo={tile} key={tile.id} />;
+        return (
+          <BeerTile
+            tileInfo={tile}
+            key={tile.id}
+            clickHandler={beerClicked}
+            hiddenTile={hiddenTile}
+          />
+        );
       })}
     </div>
   );
