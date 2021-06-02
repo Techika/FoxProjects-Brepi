29e97c9045d952f4590cc5cf0a1056dcc2d77dde
Techika <adibukovi@gmail.com>
2021-06-02 22:08:32 +0200

    Acron: Link color correction and code cleaup

--- acorn-buk/acornfun/src/App.tsx
+++ acorn-buk/acornfun/src/App.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
-// import { Link, Router } from "react-router-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navbar from "./navbar";
--- acorn-buk/acornfun/src/navbar.tsx
+++ acorn-buk/acornfun/src/navbar.tsx
@@ -1,14 +1,14 @@
-import { NavLink, BrowserRouter as Router } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = () => {
   return (
     <nav id="nav">
-      <NavLink to="/" activeClassName="active-page">
+      <NavLink exact to="/" activeClassName="active-page">
         Home page
       </NavLink>
-      <NavLink to="/simple/states" activeClassName="active-page">
+      <NavLink exact to="/simple/states" activeClassName="active-page">
         With states
       </NavLink>
     </nav>
